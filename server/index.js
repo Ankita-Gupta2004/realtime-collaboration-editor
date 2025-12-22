1468e972d4fc87b690a0bc19415c00160122033e
Ankita Gupta <ankitagupta94161@gmail.com>
2025-12-22 18:04:42 +0530

    Backend setup with Express and Socket.IO

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
